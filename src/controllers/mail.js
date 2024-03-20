888ec12d46ba28f114f91b9ee4ebaebbb1834e30
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-20 09:11:28 -0300

    refactor: Update to send emails with recipient name

--- src/controllers/mail.js
+++ src/controllers/mail.js
@@ -1,7 +1,7 @@
 import { send } from "../services/nodemailer.js"
 
 export function sendMail(req, res) {
-  const { to, subject, text } = req.body
-  send(to, subject, text)
+  const { to, name } = req.body
+  send(to, name)
   return res.status(200).json({ message: "Mail sent" })
 }
--- src/services/nodemailer.js
+++ src/services/nodemailer.js
@@ -10,11 +10,25 @@ const transporter = nodemailer.createTransport({
   }
 })
 
-export function send(to, subject, text) {
+export function send(to, name) {
   transporter.sendMail({
-    from: "contato@pipocaagil.com",
+    from: "Equipe da Simulação do Pipoca Ágil <contato@pipocaagil.com>",
     to,
-    subject,
-    text
+    subject: "Inscrição na inciativa “Simulação de Projetos Ágeis” confirmada!",
+    text: `Olá, ${name}!,
+
+    Ficamos felizes em saber que você se inscreveu com sucesso para fazer parte do voluntariado na Simulação!
+
+    É importante relembrar que a sua inscrição na inciativa não garante que você irá participar de um time.
+
+    Para fazer parte de um time, é necessário que você indique sua disponibilidade de horários e o cargo desejado no grupo de espera do WhatsApp da Simulação. Dessa forma, quando surgir uma vaga em algum grupo, você poderá ser chamado.
+
+    Clique aqui -> colocar esse link (https://chat.whatsapp.com/DzBzrELMVNZ14JaCjXOfBK) para entrar no grupo.
+
+    Caso tenha alguma dúvida, consulte nosso FAQ no site da Simulação. -> colocar esse link na frase "site da simulação" (https://pipoca-agil.netlify.app/)
+
+    Abraços,
+
+    Equipe da Simulação.`
   })
 }
