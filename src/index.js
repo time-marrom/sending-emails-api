0000dfb6c3f3160e8f88e2fcf2aa690cac4c958c
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2024-03-30 16:35:59 -0300

    fix: Update CORS configuration to only allow requests originating from http://localhost:3000

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = Number(process.env.PORT)
 const server = express()
 server.use(express.json())
 server.use(routes)
-server.use(cors({ origin: "*" }))
+server.use(cors({ origin: "http://localhost:3000" }))
 
 async function bootstrap() {
   try {
